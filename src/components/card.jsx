0e867e30d2c2922151e5525d0f49bbf8eafbe7dd
kevjinx <kevin415mei@gmail.com>
2023-01-29 09:43:20 -0800

    refactor Card to functional component

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,62 +1,46 @@
-import React, {Component} from "react";
+import React, { useState, useEffect } from "react";
 import Type from "./type.jsx";
 import getPokemonData from "../data/getPokemonData.mjs";
 import { Link } from "react-router-dom";
 
+const Card = (props) => {
+  const [pokemon, setPokemon] = useState({});
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const pokemonData = await getPokemonData(props.pokemon);
+      setPokemon(pokemonData);
+    }
+    fetchData();
+  }, [props.pokemon]);
 
-//should refactor this to a functional component
-class Card extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			pokemon: {}
-		}
-	}
+  if (!pokemon.type) {
+    return null;
+  }
 
+  return (
+    <div className="container-sm card">
+      <div className="img-container">
+        <Link to={`/pokemon/${pokemon.id}`}>
+          <img className="img-thumbnail rounded pokemon-img"src={pokemon.imgUrl} alt="test"/>
+        </Link>
+      </div>
+      <div className="container-fluid border">
+        <div className="row">
+          <div className="col">
+            <p>n {pokemon.id}</p>
+            <h4>{pokemon.name}</h4>
+          </div>
 
-	async componentDidMount() {
-		const pokemon = await getPokemonData(this.props.pokemon)
-		this.setState({pokemon})
-	}
-
-	createTypes = () => {
-		return (
-			this.state.pokemon.type.map(type => {
-				return (
-					<Type key={type} type={type}/>
-				)
-			})
-		)
-	}
-
-	render() {
-		//workaround for when state is not set when the component is rendered
-		if (!this.state.pokemon.type) {
-			return null;
-		}
-		return (
-			<div className="container-sm card">
-				<div className="img-container">
-					<Link to={`/pokemon/${this.state.pokemon.id}`}>
-						<img className="img-thumbnail rounded pokemon-img"src={this.state.pokemon.imgUrl} alt="test"/>
-					</Link>
-				</div>
-				<div className="container-fluid border">
-					<div className="row">
-						<div className="col">
-							<p>n {this.state.pokemon.id}</p>
-							<h4>{this.state.pokemon.name}</h4>
-						</div>
-
-						<div className="col">
-							{this.createTypes()}
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+          <div className="col">
+            {pokemon.type.map(type => (
+              <Type key={type} type={type}/>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
